305658badad8d200c51ae1542c6de64e79ef91da
Vincent Limo <limovincenti@gmail.com>
2025-04-03 14:07:35 +0300

    ft: added ProductDetail and Product models, to add  validation later

--- backend/app/models/Product.scala
+++ backend/app/models/Product.scala
@@ -1,5 +1,29 @@
 package models
 
-case class Product()
+import play.api.libs.json._
 
-case class Details()
+case class ProductDetail(
+	key: String,
+	value: String
+)
+
+case class Product(
+	                  id: Long,
+	                  name: String,
+	                  category: String,
+	                  code: String,
+	                  price: BigDecimal,
+	                  details: List[ProductDetail]
+                  )
+
+object Product {
+	implicit val format: OFormat[Product] = Json.format[Product]
+	
+	// TODO: add validation  	
+}
+
+object ProductDetail {
+	implicit val format: OFormat[ProductDetail] = Json.format[ProductDetail]
+	
+	// TODO: add validation  	
+}
