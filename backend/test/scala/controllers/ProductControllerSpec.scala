32e7988bd24a085f6b3aef63ecfc486cc2c684a5
Vincent Limo <limovincenti@gmail.com>
2025-04-07 18:01:19 +0300

    ft: refactor Product controller tests to remove unnecessary running app context, streamline test cases, and make them independent of db, ci should now pass

--- backend/test/scala/controllers/ProductControllerSpec.scala
+++ backend/test/scala/controllers/ProductControllerSpec.scala
@@ -1,8 +1,3 @@
-// https://www.playframework.com/documentation/3.0.x/ScalaTestingWithScalaTest
-// this test are majorly testing happy paths
-// TODO: add test to test bad request, not found etc  
-
-
 package scala.controllers
 
 import controllers.ProductController
@@ -11,11 +6,10 @@ import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito._
 import org.scalatestplus.mockito.MockitoSugar
 import org.scalatestplus.play.PlaySpec
-import play.api.Application
-import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.http.Status
 import play.api.libs.json.Json
 import play.api.mvc.{AnyContentAsEmpty, AnyContentAsJson, Result}
-import play.api.test.Helpers.{contentType, defaultAwaitTimeout, running, status}
+import play.api.test.Helpers.{contentAsJson, defaultAwaitTimeout, status}
 import play.api.test.{FakeRequest, Helpers}
 import services.IProductService
 
@@ -25,8 +19,6 @@ import scala.concurrent.Future
 class ProductControllerSpec extends PlaySpec with MockitoSugar {
 	
 	val mockProductService: IProductService = mock[IProductService]
-	
-	val app: Application = new GuiceApplicationBuilder().build()
 	
 	val controller = new ProductController(Helpers.stubControllerComponents(), mockProductService)
 	
@@ -47,78 +39,59 @@ class ProductControllerSpec extends PlaySpec with MockitoSugar {
 	"Product controller" should {
 		
 		"return health status" in {
-			running(app) {
-				val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest(Helpers.GET, "/health")
-				val result: Future[Result] = controller.health()(request)
-				status(result) mustBe Helpers.OK
-			}
+			val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest(Helpers.GET, "/health")
+			val result: Future[Result] = controller.health()(request)
+			status(result) mustBe Status.OK
 		}
 		
 		"list all products" in {
-			running(app) {
-				when(mockProductService.listProducts()) thenReturn Future.successful(List(product1, product2))
-				
-				val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest(Helpers.GET, "/products")
-				val result: Future[Result] = controller.list()(request)
-				
-				status(result) mustBe Helpers.OK
-				Helpers.contentAsJson(result).as[Seq[Product]] must contain theSameElementsAs List(product1, product2)
-			}
+			when(mockProductService.listProducts()) thenReturn Future.successful(List(product1, product2))
+			
+			val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest(Helpers.GET, "/products")
+			val result: Future[Result] = controller.list()(request)
+			
+			status(result) mustBe Status.OK
+			contentAsJson(result).as[Seq[Product]] must contain theSameElementsAs List(product1, product2)
 		}
 		
 		"get one single product by id" in {
-			running(app) {
-				
-				when(mockProductService.getProduct(1L)).thenReturn(Future.successful(Some(product1)))
-				
-				val requestFound: FakeRequest[AnyContentAsEmpty.type] = FakeRequest(Helpers.GET, "/products/1")
-				val resultFound: Future[Result] = controller.get(1L)(requestFound)
-				
-				status(resultFound) mustBe Helpers.OK
-				Helpers.contentAsJson(resultFound).as[Product] mustBe product1
-			}
+			when(mockProductService.getProduct(1L)).thenReturn(Future.successful(Some(product1)))
+			
+			val requestFound: FakeRequest[AnyContentAsEmpty.type] = FakeRequest(Helpers.GET, "/products/1")
+			val resultFound: Future[Result] = controller.get(1L)(requestFound)
+			
+			status(resultFound) mustBe Status.OK
+			contentAsJson(resultFound).as[Product] mustBe product1
 		}
 		
 		"create a new product" in {
-			running(app) {
-				
-				when(mockProductService.createProduct(any())).thenReturn(Future.successful(createdProduct))
-				
-				val request: FakeRequest[AnyContentAsJson] = FakeRequest(Helpers.POST, "/products").withJsonBody(Json.toJson(newProduct))
-				val result: Future[Result] = controller.create()(request)
-				
-				status(result) mustBe Helpers.CREATED
-				
-				Helpers.contentAsJson(result).as[Product] mustBe createdProduct
-			}
+			when(mockProductService.createProduct(any())).thenReturn(Future.successful(createdProduct))
+			
+			val request: FakeRequest[AnyContentAsJson] = FakeRequest(Helpers.POST, "/products").withJsonBody(Json.toJson(newProduct))
+			val result: Future[Result] = controller.create()(request)
+			
+			status(result) mustBe Status.CREATED
+			contentAsJson(result).as[Product] mustBe createdProduct
 		}
 		
 		"update an existing product" in {
-			running(app) {
-				
-				when(mockProductService.updateProduct(any(), any())).thenReturn(Future.successful(Some(updatedProduct)))
-				
-				val request: FakeRequest[AnyContentAsJson] =
-					FakeRequest(Helpers.PUT, "/products/1").withJsonBody(Json.toJson(updatedProduct))
-				val result: Future[Result] = controller.update(1L)(request)
-				
-				println(Helpers.contentAsString(result))
-				//status(result) mustBe Helpers.OK
-				//Helpers.contentAsJson(result).as[Product] mustBe updatedProduct
-			}
+			when(mockProductService.updateProduct(any(), any())).thenReturn(Future.successful(Some(updatedProduct)))
+			
+			val request: FakeRequest[AnyContentAsJson] =
+				FakeRequest(Helpers.PUT, "/products/1").withJsonBody(Json.toJson(updatedProduct))
+			val result: Future[Result] = controller.update(1L)(request)
+			
+			status(result) mustBe Status.OK
+			contentAsJson(result).as[Product] mustBe updatedProduct
 		}
 		
 		"delete an existing product" in {
-			running(app) {
-				
-				when(mockProductService.deleteProduct(1L)).thenReturn(Future.successful(true))
-				
-				val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest(Helpers.DELETE, "/products/1")
-				val result: Future[Result] = controller.delete(1L)(request)
-				
-				status(result) mustBe Helpers.OK
-				contentType(result) mustBe Some("application/json")
-			}
+			when(mockProductService.deleteProduct(1L)).thenReturn(Future.successful(true))
+			
+			val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest(Helpers.DELETE, "/products/1")
+			val result: Future[Result] = controller.delete(1L)(request)
+			
+			status(result) mustBe Status.OK
 		}
 	}
 }
